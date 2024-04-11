f1e95f95104c2ca2e3a71748f8bfaf50e6ab1992
Germain Baudelaire <germainjones1@gmail.com>
2024-04-11 16:20:28 +0100

    First Abstract Syntax Tree Commit

--- abstract_syntax_tree/nodes.cpp
+++ abstract_syntax_tree/nodes.cpp
@@ -3,13 +3,38 @@
 // Based on https://web.eecs.utk.edu/~bvanderz/cs461/notes/parse_tree/
 //
 
-#include <iostream>
-#include <stdlib.h>
 #include <string>
 #include <map>
 #include <list>
 #include "nodes.h"
 
 using namespace std;
+
+Channel::Channel(string value) : channel_id(value) {}
+
+SoloObjects::SoloObjects(list<Channel *> *channel_list) :
+        channels(channel_list) {}
+
+Solo::Solo(Channel *left, SoloObjects *right) :
+        channel(left), data(right) {}
+
+Send::Send(Channel *left, SoloObjects *right) : Solo(left, right) {}
+
+Receive::Receive(Channel *left, SoloObjects *right) : Solo(left, right) {}
+
+ReplicatedReceive::ReplicatedReceive(Channel *left, SoloObjects *right) :
+        Solo(left, right) {}
+
+Composition::Composition(Agent *l_agent, Agent *r_agent) :
+        left_agent(l_agent), right_agent(r_agent) {}
+
+Bind::Bind(list<Channel *> *channel_list, Agent *ag) :
+        channels(channel_list), agent(ag) {}
+
+Match::Match(list<Channel *> *c1, list<Channel *> *c2, Agent *ag) :
+        channels_1(c1), channels_2(c2), agent(ag) {}
+
+Program::Program(list<Agent *> *agent_list) :
+        agents(agent_list) {}
 
 map<string, float> idTable;
--- abstract_syntax_tree/nodes.h
+++ abstract_syntax_tree/nodes.h
@@ -6,15 +6,87 @@
 #define SOCT_NODES_H
 
 #endif //SOCT_NODES_H
+
 #include <iostream>
-#include <stdlib.h>
+#include <cstdlib>
 #include <string>
 #include <map>
 #include <list>
 
 using namespace std;
 
+class Channel {
+protected:
+    string channel_id;
+public:
+    Channel(string value);
+};
+
+class SoloObjects {
+protected:
+    list<Channel *> *channels;
+public:
+    explicit SoloObjects(list<Channel *> *channels);
+};
+
+class Agent {
+};
+
+class Inaction : public Agent {
+};
+
+class Solo : public Agent {
+public:
+    Channel *channel;
+    SoloObjects *data;
+
+    Solo(Channel *left, SoloObjects *right);
+};
+
+class Send : public Solo {
+public:
+    Send(Channel *left, SoloObjects *right);
+};
+
+class Receive : public Solo {
+public:
+    Receive(Channel *left, SoloObjects *right);
+};
+
+class ReplicatedReceive : public Solo {
+public:
+    ReplicatedReceive(Channel *left, SoloObjects *right);
+};
+
+class Composition : public Agent {
+protected:
+    Agent *left_agent, *right_agent;
+public:
+    Composition(Agent *l_agent, Agent *r_agent);
+};
+
+class Bind : public Agent {
+protected:
+    list<Channel *> *channels;
+    Agent *agent;
+public:
+    Bind(list<Channel *> *channel_list, Agent *ag);
+};
+
+class Match : public Agent {
+protected:
+    list<Channel *> *channels_1;
+    list<Channel *> *channels_2;
+    Agent *agent;
+public:
+    Match(list<Channel *> *c1, list<Channel *> *c2, Agent *ag);
+};
+
 class Program {
+protected:
+    list<Agent *> *agents;
+public:
+    explicit Program(list<Agent *> *agent_list);
 };
 
 // the object at the base of our tree
